7eb02b9e4affedf28f20b23d607cd58ca755532e
ALEXANDER-PC <alex.freiberger@gmx.de>
2018-04-09 01:47:32 +0200

    AIBase

---
+++ Assets/AITestOnline/AIBase.cs
@@ -0,0 +1,31 @@
+﻿/******************************************
+*                                         *
+*   Script made by Alexander Freiberger   *
+*                                         *
+*   Edited by:                            *
+*                                         *
+******************************************/
+using UnityEngine.Networking;
+
+public abstract class AIBase : NetworkBehaviour 
+{
+    /// <summary>
+    /// To be executed when NPC is spawned.
+    /// </summary>
+    public abstract void OnNPCSpawn();
+
+    /// <summary>
+    /// Method to handle the death of the NPC
+    /// </summary>
+    public abstract void KillNPC();
+
+    /// <summary>
+    /// To be executed when NPC is dying, handles loot drops and similar.
+    /// </summary>
+    public abstract void OnNPCDeath();
+
+    /// <summary>
+    /// Handles the Decisionmaking of the NPC based on current State and possible options.
+    /// </summary>
+    public abstract void NPCDecision();
+}
---
+++ Assets/AITestOnline/AIEnums.cs
@@ -0,0 +1,20 @@
+﻿/******************************************
+*                                         *
+*   Script made by Alexander Freiberger   *
+*                                         *
+*   Edited by:                            *
+*                                         *
+******************************************/
+
+
+[System.Flags]
+public enum AIState
+{
+    DEAD = 0,
+    ALIVE = 1,
+    IDLE = 2,
+    MOVING = 4,
+    ATTACKING = 8,
+    DYING = 16,
+
+}
