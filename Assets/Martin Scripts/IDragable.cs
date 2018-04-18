308ee03473758049c28a53909e450596eae31cc5
MARTIN\Martin <martin.lohse.91@firemail.de>
2018-04-18 18:00:22 +0200

    Added Drag and Drop! (Noch kleinere Fehler. Ggf. auch größere)

---
+++ Assets/Martin Scripts/IDragable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// The spelling is on purpose!
+public interface IDragable
+{
+    //	#########################################
+    //	O			IDragable				    O
+    //	O---------------------------------------O
+    //	O	Author:	Martin Lohse				O
+    //	O	Date: 18.04.2018					O
+    //	O	Edited: X							O
+    //	O	Description: This is an Interface	O
+    //	O	             for the dragable Items	O
+    //	O---------------------------------------O
+    //	O	Name:								O
+    //	O	Date:								O
+    //  O 	Chanes:								O
+    //	O---------------------------------------O
+    //	O										O
+    //	#########################################
+
+    GameObject gameObject { get; }
+}
